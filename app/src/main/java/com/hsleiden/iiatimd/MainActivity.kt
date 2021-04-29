dee2c429d29d06da619438bd060cd5a97a2a0293
Lars <lvdnbusiness@gmail.com>
2021-04-29 12:15:05 +0200

    Added new images/colors

--- app/src/main/java/com/hsleiden/iiatimd/MainActivity.kt
+++ app/src/main/java/com/hsleiden/iiatimd/MainActivity.kt
@@ -1,40 +1,122 @@
 package com.hsleiden.iiatimd
 
+import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var onboardingItemsAdapter: OnboardingItemsAdapter
+    private lateinit var indicatorsContainer: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setOnboardingItems()
+        setupIndicators()
+        setCurrentIndicator(0)
     }
 
     private fun setOnboardingItems() {
         onboardingItemsAdapter = OnboardingItemsAdapter(
             listOf(
                 OnboardingItem(
-                    onboardingImage = R.drawable.mobile_app,
-                    title = "Inloggen met je Hogeschool account",
-                    description = "Dit systeem is gekoppeld aan dat van de Hogeschool Leiden, waardoor jij veilig en makkelijk kan inloggen."
+                    onboardingImage = R.drawable.card,
+                    title = "Collegekaart synchroniseren",
+                    description = "Door simpelweg je collegekaart tegen je telefoon aan te houden kan je hem gemakkelijk aan je account toevoegen."
                 ),
                 OnboardingItem(
-                    onboardingImage = R.drawable.cloud_sync,
-                    title = "Synchroniseer je studentenpas",
-                    description = "Door simpelweg je schoolpas tegen je telefoon aan te houden kan je hem gemakkelijk aan je account toevoegen."
+                    onboardingImage = R.drawable.mobile_login,
+                    title = "Hogeschool account koppelen",
+                    description = "Dit systeem is gekoppeld aan dat van de Hogeschool Leiden, waardoor je slechts met een paar knoppen kan inloggen."
                 ),
                 OnboardingItem(
-                    onboardingImage = R.drawable.secure_data,
-                    title = "Beveiliging is onze prioriteit",
-                    description = "Er worden zo min mogelijk gegevens van jou opgeslagen. De gegevens die we wel opslaan, versleutelen we en verwijderen we bij inactiviteit."
+                    onboardingImage = R.drawable.secure_server,
+                    title = "Veiligheid is onze prioriteit",
+                    description = "Er worden zo min mogelijk gegevens van jou opgeslagen. Wat we wel moeten opslaan, wordt goed versleuteld."
                 ),
             )
         )
         val onboardingViewPager = findViewById<ViewPager2>(R.id.onboardingViewPager)
         onboardingViewPager.adapter = onboardingItemsAdapter
+        onboardingViewPager.registerOnPageChangeCallback(object :
+            ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                setCurrentIndicator(position)
+            }
+        })
+        (onboardingViewPager.getChildAt(0) as RecyclerView).overScrollMode =
+            RecyclerView.OVER_SCROLL_NEVER
+        findViewById<ImageView>(R.id.imageNext).setOnClickListener {
+            if (onboardingViewPager.currentItem + 1 < onboardingItemsAdapter.itemCount) {
+                onboardingViewPager.currentItem += 1
+            } else {
+                navigateToHomeActivity()
+            }
+        }
+        findViewById<TextView>(R.id.textSkip).setOnClickListener {
+            navigateToHomeActivity()
+        }
+        findViewById<MaterialButton>(R.id.buttonGetStarted).setOnClickListener {
+            navigateToHomeActivity()
+        }
     }
+
+    private fun navigateToHomeActivity() {
+        startActivity(Intent(applicationContext, HomeActivity::class.java))
+        finish()
+    }
+
+    private fun setupIndicators() {
+        indicatorsContainer = findViewById(R.id.indicatorsContainer)
+        val indicators = arrayOfNulls<ImageView>(onboardingItemsAdapter.itemCount)
+        val layoutParams: LinearLayout.LayoutParams = LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+        layoutParams.setMargins(8, 0, 8, 0)
+        for (i in indicators.indices) {
+            indicators[i] = ImageView(applicationContext)
+            indicators[i]?.let {
+                it.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_inactive_background
+                    )
+                )
+                it.layoutParams = layoutParams
+                indicatorsContainer.addView(it)
+            }
+        }
+    }
+
+    private fun setCurrentIndicator(position: Int) {
+        val childCount = indicatorsContainer.childCount
+        for (i in 0 until childCount) {
+            val imageView = indicatorsContainer.getChildAt(i) as ImageView
+            if (i == position) {
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_active_background
+                    )
+                )
+            } else {
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_inactive_background
+                    )
+                )
+            }
+        }
+    }
+
 }
